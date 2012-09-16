f258a86f3102fe6c157e0298d6d189da3211b932
Bob Heckel <rsh@rshdev.com>
2012-09-16 13:55:22 -0400

    mod

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,14 +4,22 @@
 #
 #  Summary: Build dotfile (and configuration) symlinks to ~ on a new box
 #
+#           Cygwin prerequisite:
+#           mount -f u:/code /home/rsh86800/code
+#           mount -m >> /etc/fstab
+#
 #  Created: Sat 17 Mar 2012 14:13:44 (Bob Heckel)
-# Modified: Sun 06 May 2012 13:32:15 (Bob Heckel)
+# Modified: Tue 28 Aug 2012 13:35:21 (Bob Heckel)
 ##############################################################################
 
+# Set to empty for real run
 dryrun=echo
 symloc=$HOME/code/misccode
 
 echo "!!! dryrun set to $dryrun !!!!"
+echo "!!! dryrun set to $dryrun !!!!"
+echo "!!! dryrun set to $dryrun !!!!"
+echo 'ok? '
 read
 echo "'cannot stat' errors are to be expected"
 
@@ -22,22 +30,23 @@ echo 'starting underscore symlinking...'
 for f in ${underscores[*]}; do
   $dryrun mv -i $HOME/.$f $HOME/.$f.ORIG  # backup just in case
   $dryrun ln -s $symloc/_$f $HOME/.$f     # then overwrite it
+  echo
 done
 
 
-echo; echo 'starting fluxbox symlinking...'
-flux=(keys menu init startup)
-for f in ${flux[*]}; do
-  $dryrun mv -i $HOME/.fluxbox/$f $HOME/.fluxbox/$f.ORIG  # backup just in case
-  $dryrun ln -s $symloc/fluxbox.$f $HOME/.fluxbox/$f      # then overwrite it
-done
-
-
-echo; echo 'starting xfe copy (must copy because xfe will overwrite symlinks)...'
-$dryrun mv -i $HOME/.config/xfe/xferc $HOME/.config/xferc/xferc.ORIG  # backup just in case
-$dryrun cp -i $HOME/code/misccode/xferc $HOME/.config/xferc/xferc     # then overwrite it
-
-
-echo; echo 'starting conky symlinking...'
-$dryrun mv -i $HOME/.conkyrc $HOME/.conkyrc.ORIG  # backup just in case
-$dryrun ln -s $symloc/_conkyrc $HOME/.conkyrc     # then overwrite it
+###echo; echo 'starting fluxbox symlinking...'
+###flux=(keys menu init startup)
+###for f in ${flux[*]}; do
+###  $dryrun mv -i $HOME/.fluxbox/$f $HOME/.fluxbox/$f.ORIG  # backup just in case
+###  $dryrun ln -s $symloc/fluxbox.$f $HOME/.fluxbox/$f      # then overwrite it
+###done
+###
+###
+###echo; echo 'starting xfe copy (must copy because xfe will overwrite symlinks)...'
+###$dryrun mv -i $HOME/.config/xfe/xferc $HOME/.config/xferc/xferc.ORIG  # backup just in case
+###$dryrun cp -i $HOME/code/misccode/xferc $HOME/.config/xferc/xferc     # then overwrite it
+###
+###
+###echo; echo 'starting conky symlinking...'
+###$dryrun mv -i $HOME/.conkyrc $HOME/.conkyrc.ORIG  # backup just in case
+###$dryrun ln -s $symloc/_conkyrc $HOME/.conkyrc     # then overwrite it
