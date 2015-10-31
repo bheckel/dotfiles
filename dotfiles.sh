72d36bf26097509f34291187e0f538bdb1fc6fa4
Bob Heckel <rsh@rshdev.com>
2015-10-31 17:55:48 -0400

    mod

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,9 @@
 
 cd ~
 
+if [ ! -e ~/bin ]; then mkdir ~/bin; fi
+if [ ! -e ~/tmp ]; then mkdir ~/tmp; fi
+
 mv -i .bashrc .bashrc.ORIG
 ln -s ./dotfiles/_bashrc .bashrc
 
@@ -10,5 +13,8 @@ ln -s ./dotfiles/_vimrc .vimrc
 
 mv -i .inputrc .inputrc.ORIG
 ln -s ./dotfiles/_inputrc .inputrc
+
+mv -i .tmux.conf .tmux.conf.ORIG
+ln -s ./dotfiles/_tmux.conf .tmux.conf
 
 echo completed symlinking
