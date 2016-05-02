33ed1426fbc49153697bf46bc0df6efa7e3d47a2
Bob.Heckel <rsh@rshdev.com>
2016-05-02 15:14:29 -0400

    use loops, more setup steps

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-cd ~
+# Modified: Mon 02 May 2016 13:39:43 (Bob Heckel)
 
-if [ ! -e ~/bin ]; then mkdir ~/bin; fi
-if [ ! -e ~/tmp ]; then mkdir ~/tmp; fi
+echo starting dotfile setup in $HOME...
+cd $HOME
+ls -a $HOME
+echo -n 'ok? Ctl-c to cancel '
+read
 
-mv -i .bashrc .bashrc.ORIG
-ln -s ./dotfiles/_bashrc .bashrc
 
-mv -i .vimrc .vimrc.ORIG
-ln -s ./dotfiles/_vimrc .vimrc
+echo 'setting up directories (if needed)...'
+if [ ! -e $HOME/bin ]; then mkdir $HOME/bin; fi
+if [ ! -e $HOME/tmp ]; then mkdir $HOME/tmp; fi
+if [ ! -e $HOME/code/misccode ]; then mkdir -p $HOME/code/misccode; fi
 
-mv -i .inputrc .inputrc.ORIG
-ln -s ./dotfiles/_inputrc .inputrc
+echo 'setting up oneliners...'
+cp -i $HOME/dotfile/oneliners $HOME/code/misccode/
 
-mv -i .tmux.conf .tmux.conf.ORIG
-ln -s ./dotfiles/_tmux.conf .tmux.conf
+# No leading dots!
+for f in bashrc vimrc inputrc tmux.conf; do echo $f;
+  echo setting up $f...
+  if [ -e $HOME/.$f ]; then
+    mv -i $HOME/.$f $HOME/.$f.ORIG
+    echo .$f was backed up as .$f.ORIG
+  else
+    echo $f does not yet exist
+  fi
+###  echo "source $HOME/dotfiles/_$f" >| ~/$f
+  ln -s $HOME/dotfiles/_$f $HOME/.$f
+  ls -la $HOME/.$f*
+done
 
-echo completed symlinking
+echo ...completed setup
+
