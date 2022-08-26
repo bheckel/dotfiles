dd9e0c1b1b4d874a719adfb55efa375f80045f6a
Bob Heckel <rsh@rshdev.com>
2022-08-26 16:17:35 -0400

    mod gitall x86_64-unknown-cygwin-l10g

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  Created: 03-May-2015 (Bob Heckel) 
-# Modified: 08-Apr-2022 (Bob Heckel)
+# Modified: 26-Aug-2022 (Bob Heckel)
 
 # No leading dots!
 if [ -z WSL_DISTRO_NAME ]; then
@@ -10,6 +10,10 @@ else
   majordots='bashrc vimrc tmux.conf inputrc gitconfig'
 fi
   
+myhome=$HOME
+# Debug toggle
+#myhome=$(pwd)
+
 if [ "$#" -ge 1 ]; then 
   cat <<EOT
 Usage: $0
@@ -23,13 +27,6 @@ EOT
   exit 1
 fi
 
-if [ -n "$1" ]; then
-  # Debugging
-  myhome=$1
-else
-  myhome=$HOME
-fi
-
 cd $myhome
 
 echo
@@ -38,15 +35,14 @@ read
 echo
 
 for f in $majordots; do
-  echo '........................'
-  echo setting up $myhome/.$f...
+  echo -n "setting up $myhome/.$f .............	 "
   if [ -e $myhome/.$f ] || [ -L $myhome/.$f ]; then
-    mv -i $myhome/.$f $myhome/.$f.ORIG
-    echo .$f was backed-up as .$f.ORIG
-  else
-    echo .$f does not yet exist
+    mv $myhome/.$f $myhome/.$f.ORIG
+    #echo .$f was backed-up as .$f.ORIG
+  #else
+    #echo .$f does not yet exist
   fi
-  ln -s $myhome/dotfiles/_$f $myhome/.$f
+  ln -s $myhome/dotfiles/_$f $myhome/.$f && echo "[${fg_green}  OK  ${normal}]"
 done
 
 # 24-Aug-19 works on Oracle VM so commenting out
@@ -67,19 +63,14 @@ if [ ! -e $HOME/tmp ]; then
   mkdir $HOME/tmp
 fi
 
-#%ls -la ~
+if [ -e /cygdrive ]; then
+  mkdir -p /cygdrive/c/temp
+fi
 
 echo
 echo "Consider installing:"
-echo "$ sudo apt-get install tmux vim-nox w3m bc git ssh"
+echo "$ sudo apt-get install tmux vim-nox w3m bc ssh"
 echo 'or'
-echo "$ sudo apt-get install tmux vim-gtk3 w3m bc git ssh"
+echo "$ sudo apt-get install tmux vim-gtk3 w3m bc ssh"
 echo
-
-echo 'Consider running:'
-if [ -e /cygdrive ]; then
-  echo '$ mkdir -p /cygdrive/c/temp'
-fi
-echo 'git config --global user.email "b.heckel@gmail.com"'
-echo 'git config --global user.name "Bob Heckel"'
 
