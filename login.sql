cd70985b0ccfe6c3498d0680f5acd6ed7e031358
Bob Heckel <b.heckel@gmail.com>
2025-08-14 09:58:00 -0400

    mod gitall x86_64-redhat-linux-gnu-webs

--- login.sql
+++ login.sql
@@ -1,8 +1,9 @@
+DEFINE _EDITOR=vim
 set cloudconfig wallet.zip
 set define off
 -- Do not autocommit on Ctl-C crashouts
 set exitcommit OFF
 set sqlformat ANSICONSOLE
 set timing ON
-set serveroutput ON size 500000
-set sqlprompt "@|white _USER|@@@|green _CONNECT_IDENTIFIER|@@|white > |@"
+set serveroutput ON size 100000
+--set sqlprompt "@|white _USER|@@@|green _CONNECT_IDENTIFIER|@@|white > |@"
